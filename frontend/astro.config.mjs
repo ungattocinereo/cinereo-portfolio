f4921539c84aedc80121ae671c03fb4400905bb1
greg <greg@cinereo.it>
2025-11-20 17:21:49 +0000

    Changes before Firebase Studio auto-run

---
+++ frontend/astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@astrojs/vue';
+import svgLoader from 'vite-svg-loader';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [
+        tailwindcss(),
+        svgLoader({
+            defaultImport: 'component'
+        })
+    ]
+  },
+
+  integrations: [vue()]
+});
