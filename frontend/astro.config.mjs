9154d46d35a60c62671fb6ff259a76d843b02310
greg <greg@cinereo.it>
2025-11-20 18:03:51 +0000

    Add Vercel adapter

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,12 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@astrojs/vue';
 import svgLoader from 'vite-svg-loader';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   vite: {
     plugins: [
         tailwindcss(),
